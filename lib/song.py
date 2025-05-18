4a07820f64ff0e0ec9802eb0ef6c744b738796da
william <williamwambugu663@gmail.com>
2025-05-18 17:13:52 +0300

    Initial commit

--- lib/song.py
+++ lib/song.py
@@ -1,2 +1,46 @@
 class Song:
-    pass
+    count = 0
+    genres = []
+    artists = []
+    genre_count = {}
+    artist_count = {}
+
+    def __init__(self, name, artist, genre):
+        self.name = name
+        self.artist = artist
+        self.genre = genre
+
+        # Add to song count
+        Song.count += 1
+
+        # Track unique genres
+        if genre not in Song.genres:
+            Song.genres.append(genre)
+
+        # Track unique artists
+        if artist not in Song.artists:
+            Song.artists.append(artist)
+
+        # Update genre count
+        if genre in Song.genre_count:
+            Song.genre_count[genre] += 1
+        else:
+            Song.genre_count[genre] = 1
+
+        # Update artist count
+        if artist in Song.artist_count:
+            Song.artist_count[artist] += 1
+        else:
+            Song.artist_count[artist] = 1
+
+ninety_nine_problems = Song("99 Problems", "Jay-Z", "Rap")
+
+print(ninety_nine_problems.name) 
+print(ninety_nine_problems.artist)     
+print(ninety_nine_problems.genre)     
+
+print(Song.count)                     
+print(Song.genres)                     
+print(Song.artists)                    
+print(Song.genre_count)                
+print(Song.artist_count)               
